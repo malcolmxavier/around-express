adabefb0a2c25f7088e38658307b873c82c2d1e5
Malcolm Xavier <malcolm.x.evans@gmail.com>
2020-10-04 15:40:48 -0400

    fix server error code

--- server/helpers/getFileContent.js
+++ server/helpers/getFileContent.js
@@ -3,7 +3,7 @@ const fs = require('fs').promises;
 function getFileContent(path, req, res) {
   return fs.readFile(path, { encoding: 'utf-8' })
     .then(JSON.parse)
-    .catch(res.status(404).send({ message: 'Requested Resource not found' }));
+    .catch(res.status(500).send({ message: 'Requested Resource not found' }));
 }
 
 module.exports = getFileContent;
