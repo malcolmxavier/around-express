da0dba220f4d53aaeeacef14f8a2e4a9b4fa887d
Malcolm Xavier <malcolm.x.evans@gmail.com>
2020-10-25 10:15:22 -0400

    fix error codes and user controller functions

--- server/controllers/cardController.js
+++ server/controllers/cardController.js
@@ -1,25 +1,22 @@
-const Card = require('../models/card');
+import StatusCodes from 'http-status-codes';
 
-const SUCCESS_CODE = 200;
-const INVALID_ERROR_CODE = 400;
-const NOT_FOUND_ERROR_CODE = 404;
-const DEFAULT_ERROR_CODE = 500;
+const Card = require('../models/card');
 
 function getCards(req, res) {
   return Card.find({})
-    .then((users) => res.status(SUCCESS_CODE).send(users))
-    .catch((err) => res.status(INVALID_ERROR_CODE).send(err));
+    .then((users) => res.status(StatusCodes.OK).send(users))
+    .catch((err) => res.status(StatusCodes.BAD_REQUEST).send(err));
 }
 
 function createCard(req, res) {
   const { name, link } = req.body;
   Card.create({ name, link, owner: req.user._id })
-    .then((card) => res.status(SUCCESS_CODE).send({ data: card }))
+    .then((card) => res.status(StatusCodes).OK.send({ data: card }))
     .catch((err) => {
       if (err.name === 'ValidationError') {
-        res.status(INVALID_ERROR_CODE).send({ message: 'Card validation failed' });
+        res.status(StatusCodes.BAD_REQUEST).send({ message: 'Card validation failed' });
       } else {
-        res.status(DEFAULT_ERROR_CODE).send({ message: 'Internal server error' });
+        res.status(StatusCodes.INTERNAL_SERVER_ERROR).send({ message: 'Internal server error' });
       }
     });
 }
@@ -27,26 +24,26 @@ function createCard(req, res) {
 function deleteCard(req, res) {
   const { id } = req.params.cardId;
   return Card.deleteOne(id)
-    .then((card) => res.status(SUCCESS_CODE).send({ card, message: 'Card has been deleted' }))
+    .then((card) => res.status(StatusCodes.OK).send({ card, message: 'Card has been deleted' }))
     .catch((err) => {
       if (err.name === 'CastError') {
-        res.status(NOT_FOUND_ERROR_CODE).send({ message: 'The requested card was not found' });
+        res.status(StatusCodes.NOT_FOUND).send({ message: 'The requested card was not found' });
       } else {
-        res.status(DEFAULT_ERROR_CODE).send({ message: 'Internal server error' });
+        res.status(StatusCodes.INTERNAL_SERVER_ERROR).send({ message: 'Internal server error' });
       }
     });
 }
 
 function addCardLike(req, res) {
   Card.findByIdAndUpdate(req.params.cardId, { $addToSet: { likes: req.user._id } }, { new: true })
-    .then((user) => res.status(SUCCESS_CODE).send(user))
-    .catch(res.status(DEFAULT_ERROR_CODE).send({ message: 'Internal server error' }));
+    .then((user) => res.status(StatusCodes.OK).send(user))
+    .catch(res.status(StatusCodes.INTERNAL_SERVER_ERROR).send({ message: 'Internal server error' }));
 }
 
 function removeCardLike(req, res) {
   Card.findByIdAndUpdate(req.params.cardId, { $pull: { likes: req.user._id } }, { new: true })
-    .then((user) => res.status(SUCCESS_CODE).send(user))
-    .catch(res.status(DEFAULT_ERROR_CODE).send({ message: 'Internal server error' }));
+    .then((user) => res.status(StatusCodes.OK).send(user))
+    .catch(res.status(StatusCodes.INTERNAL_SERVER_ERROR).send({ message: 'Internal server error' }));
 }
 
 module.exports = {
--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,51 +1,51 @@
-const User = require('../models/user');
+import { StatusCodes } from 'http-status-codes';
 
-const SUCCESS_CODE = 200;
-const INVALID_ERROR_CODE = 400;
-const NOT_FOUND_ERROR_CODE = 404;
-const DEFAULT_ERROR_CODE = 500;
+const User = require('../models/user');
 
 function getUsers(req, res) {
   return User.find({})
-    .then((users) => res.status(SUCCESS_CODE).send(users))
-    .catch((err) => res.status(INVALID_ERROR_CODE).send(err));
+    .then((users) => res.status(StatusCodes.OK).send(users))
+    .catch((err) => res.status(StatusCodes.BAD_REQUEST).send(err));
 }
 
 function getOneUser(req, res) {
-  return User.find({ id: req.params.id })
-    .then((user) => {
-      if (user) {
-        return res.status(SUCCESS_CODE).send(user);
+  return User.find({ _id: req.params._id })
+    .then((user) => res.status(StatusCodes.OK).send(user))
+    .catch((err) => {
+      if (err.name === 'DocumentNotFoundError') {
+        res.status(StatusCodes.NOT_FOUND).send({ message: 'User ID not found.' });
+      } else {
+        res.status(StatusCodes.BAD_REQUEST).send(err);
       }
-
-      return res.status(NOT_FOUND_ERROR_CODE).send({ message: 'User ID not found.' });
-    })
-    .catch((err) => res.status(INVALID_ERROR_CODE).send(err));
+    });
 }
 
 function createUser(req, res) {
+  const { name, about, avatar } = req.body;
   return User.countDocuments({})
-    .then((id) => User.create(...req.body, id))
-    .then((user) => res.status(SUCCESS_CODE).send(user))
+    .then((_id) => User.create({
+      name, about, avatar, _id,
+    }))
+    .then((user) => res.status(StatusCodes.OK).send(user))
     .catch((err) => {
       if (err.name === 'ValidationError') {
-        res.status(INVALID_ERROR_CODE).send({ message: 'User validation failed' });
+        res.status(StatusCodes.BAD_REQUEST).send({ message: 'User validation failed' });
       } else {
-        res.status(DEFAULT_ERROR_CODE).send({ message: 'Internal server error' });
+        res.status(StatusCodes.INTERNAL_SERVER_ERROR).send({ message: 'Internal server error' });
       }
     });
 }
 
 function updateUser(req, res) {
   return User.findByIdAndUpdate(req.params._id, { name: req.body.name, about: req.body.about }
-    .then((user) => res.status(SUCCESS_CODE).send(user))
-    .catch(res.status(DEFAULT_ERROR_CODE).send({ message: 'Internal server error' })));
+    .then((user) => res.status(StatusCodes.OK).send(user))
+    .catch(res.status(StatusCodes.INTERNAL_SERVER_ERROR).send({ message: 'Internal server error' })));
 }
 
 function updateUserAvatar(req, res) {
   return User.findByIdAndUpdate(req.params._id, { avatar: req.body.avatar }
-    .then((user) => res.status(SUCCESS_CODE).send(user))
-    .catch(res.status(DEFAULT_ERROR_CODE).send({ message: 'Internal server error' })));
+    .then((user) => res.status(StatusCodes.OK).send(user))
+    .catch(res.status(StatusCodes.INTERNAL_SERVER_ERROR).send({ message: 'Internal server error' })));
 }
 
 module.exports = {
